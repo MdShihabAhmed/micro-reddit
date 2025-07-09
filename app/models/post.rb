5d209fa6446d6c1df605fa34809db95f6b1c3f64
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-09 11:13:06 +0600

    model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
+
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
