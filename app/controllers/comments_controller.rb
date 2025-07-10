cd656c25b630a904b0282625f6453070c24bd20f
Md Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-10 15:29:01 +0600

    can select from available users while commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,12 @@ class CommentsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    @comment.user_id=params[:user_id]
-    @comment.save
+    @comment = @post.comments.create(comment_params)
     redirect_to user_post_path(@user, @post)
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body,:user_id)
     end
 end
