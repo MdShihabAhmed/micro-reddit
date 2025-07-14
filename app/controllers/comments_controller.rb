007652446df8b2837b8e759b2891a36ded5d98d5
Md Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-14 16:44:37 +0600

    can not edit post without the current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
   def create
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to user_post_path(@user, @post)
