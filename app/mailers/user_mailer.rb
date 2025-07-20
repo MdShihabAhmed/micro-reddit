e5dc42b4b59971ab65e06ae857994766dc4f01e1
Md Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-20 17:13:21 +0600

    removed unused line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url  = "http://example.com/login"
     mail(to: @user.email, subject: "Welcome to Micro-Reddit")
   end
 end
